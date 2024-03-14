c68cd2ca5ffd326d08b063225e07c78dd7b3d626
GyodaiDDA <rodrigo.gyodai@gmail.com>
2024-03-14 03:01:28 -0300

    Sistema de busca corrigido
    pŕoximo:
    . criação de endpoint para importação de csv
    . implementação de assincronicidade do procedimento
    . criação de página para importação de csv

--- helpers/endpoint_helper.rb
+++ helpers/endpoint_helper.rb
@@ -6,14 +6,15 @@ def get_exams_by_tokens(conn, params)
   query = File.open("queries/exams_by_token.sql").read
   results = []
   tokens = params['tokens'].split(',')
-  tokens.each do |token|
-    results << conn.exec_params(query, [token]).entries
+  tokens.uniq.each do |token|
+    results << conn.exec_params(query, [token]).entries[0]
   end
-  results.reject! { |result| result.empty? }
+  results = results.reject { |result| result.nil? || result.empty? }
   halt(404,{'Content-Type' => 'application/json'}, 'Token não encontrado') if results.empty?
+
   data = {results: }
   data[:results].each do |row|
-    %w[doctor tests].each { |key| row[0][key] = JSON.parse(row[0][key]) if row[0][key] }
+    %w[doctor tests].each { |key| row[key] = JSON.parse(row[key]) if row[key] }
   end
   data
 end
--- public/main.js
+++ public/main.js
@@ -1,62 +1,26 @@
 const fragment = document.createDocumentFragment();
-const list_url = `http://localhost:4567/tests`;
+const listURL = `http://localhost:4567/tests`;
 const token = '';
-const token_url = `http://localhost:4567/tests/${token}`;
+const searchURL = `http://localhost:4567/tests/${token}`;
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const listButton = document.getElementById('list-button');
 
-listButton.addEventListener('click', showList);
+listButton.addEventListener('click', function() {
+  showList(listURL);
+});
 
 searchButton.addEventListener('click', function() {
-  console.log('search button clicked');
   const searchToken = searchInput.value;
-  searchData(searchToken);
+  showList(searchURL + searchToken);
 });
 
 
-  function showList() {
-    fetch(list_url).
-      then((response) => response.json()).
-      then((data) => populateList(data)).
-      then(() => {
-        document.querySelector('ul').appendChild(fragment);
-      }).
-      catch(function(error) {
-        console.log(error);
-      });
-  };
-
-  function populateList(data) {
-    data.results.forEach(function(result) {
-      const li = document.createElement('li');
-      li.classList.add('list-item');
-      appendElement('span', result.result_token, li, 'short-text-component', "token");
-      appendElement('span', result.result_date, li, 'short-text-component', "exam date:");
-      li.addEventListener('click', function() {
-        showDetails(result, li);
-      });
-      fragment.appendChild(li);
-    })
-  }
-
-  function searchData(token) {
-  fetch(`${token_url}${token}`).
+function showList(source) {
+  clearList();
+  fetch(source).
     then((response) => response.json()).
-    then((data) => {
-      console.log(data)
-      data.results.forEach(function(result) {
-        const li = document.createElement('li');
-        li.classList.add('list-item');
-        appendElement('div', '', li);
-        appendElement('span', result.result_token, li, 'short-text-component', "token");
-        appendElement('span', result.result_date, li, 'short-text-component', "exam date:");
-        li.addEventListener('click', function() {
-          showDetails(result, li);
-        });
-        fragment.appendChild(li);
-      })
-    }).
+    then((data) => populateList(data)).
     then(() => {
       document.querySelector('ul').appendChild(fragment);
     }).
@@ -65,64 +29,84 @@ searchButton.addEventListener('click', function() {
     });
 };
 
-  function appendElement(elementType, text, target, aclass = null, label = null){
-    const element = document.createElement(elementType);
-    addClass(element,`${aclass}`);
-    if (label) {
-      element.textContent = `${label} ${text}`;
-    } else {
-      element.textContent = text;
-    }
-    target.appendChild(element);
-  }
+function clearList() {
+  const details = document.querySelectorAll('.details');
+  details.forEach(item => item.remove());
+  const listItems = document.querySelectorAll('.list-item');
+  listItems.forEach(item => item.remove());
+}
 
-  function appendTable(list, item, item_params, target){
-    const table = document.createElement('table');
-    table.classList.add('short-table');
-    list.forEach(function(item){
-      const tr = document.createElement('tr');
-      item_params.forEach(function(param) {
-        appendElement('td',item[param].toUpperCase(), tr, `${param}:`)
-      }),      
-      table.appendChild(tr);
-    }),
-    target.appendChild(table);
-  }
-
-
-  function showDetails(result, container) {
-    if (!container.classList.contains('selected')) {
-      hideAll('.details');
-    }
-    if (container.nextElementSibling.classList.contains('details')) {
-      let detailsDiva = container.nextElementSibling;
-      detailsDiva.style.display = detailsDiva.style.display === 'none' ? 'block' : 'none';
-    } else {
-      detailsDiv = document.createElement('div');
-      detailsDiv.classList.add('details');
-      appendElement('p', result.name.toUpperCase(), detailsDiv, "patient:");
-      appendElement('p', result.doctor.name.toUpperCase(), detailsDiv, "doctor:");
-      appendTable(result.tests, 'test', ['test', 'limits','result'], detailsDiv, "tests:");
-      container.after(detailsDiv);
-      detailsDiv.style.display = 'block';
-    }
-    container.classList.toggle('selected');
-  }
-
-  function hideAll(selector) {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach(element => {
-      if (element.style.display === 'block') {
-        element.style.display = 'none';
-        element.previousElementSibling.classList.remove('selected');
-      }
+function populateList(data) {
+  data.results.forEach(function(result) {
+    console.log(result);
+    const li = document.createElement('li');
+    addClass(li,'list-item');
+    appendElement('span', result.result_token, li, 'short-text-component', "token");
+    appendElement('span', result.result_date, li, 'short-text-component', "exam date:");
+    li.addEventListener('click', function() {
+      showDetails(result, li);
     });
-  }
+    fragment.appendChild(li);
+  })
+}
 
-  function addClass(element, className) {
-    if (element.classList) {
-        element.classList.add(className);
-    } else {
-        element.className += ' ' + className;
-    }
+function showDetails(result, container) {
+  if (!container.classList.contains('selected')) {
+    hideAll('.details');
   }
+  if (container.nextElementSibling && container.nextElementSibling.classList.contains('details')) {
+    let detailsDiva = container.nextElementSibling;
+    detailsDiva.style.display = detailsDiva.style.display === 'none' ? 'block' : 'none';
+  } else {
+    detailsDiv = document.createElement('div');
+    detailsDiv.classList.add('details');
+    appendElement('p', result.name.toUpperCase(), detailsDiv, "patient:");
+    appendElement('p', result.doctor.name.toUpperCase(), detailsDiv, "doctor:");
+    appendTable(result.tests, 'test', ['test', 'limits','result'], detailsDiv, "tests:");
+    container.after(detailsDiv);
+    detailsDiv.style.display = 'block';
+  }
+  container.classList.toggle('selected');
+}
+
+function hideAll(selector) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach(element => {
+    if (element.style.display === 'block') {
+      element.style.display = 'none';
+      element.previousElementSibling.classList.remove('selected');
+    }
+  });
+}
+
+function appendElement(elementType, text, target, aclass = null, label = null){
+  const element = document.createElement(elementType);
+  addClass(element,`${aclass}`);
+  if (label) {
+    element.textContent = `${label} ${text}`;
+  } else {
+    element.textContent = text;
+  }
+  target.appendChild(element);
+}
+
+function appendTable(list, item, item_params, target){
+  const table = document.createElement('table');
+  addClass(table,'short-table');
+  list.forEach(function(item){
+    const tr = document.createElement('tr');
+    item_params.forEach(function(param) {
+      appendElement('td',item[param].toUpperCase(), tr, `${param}:`)
+    }),      
+    table.appendChild(tr);
+  }),
+  target.appendChild(table);
+}
+
+function addClass(element, className) {
+  if (element.classList) {
+      element.classList.add(className);
+  } else {
+      element.className += ' ' + className;
+  }
+}
